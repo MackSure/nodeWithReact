97cc567112312179ba20190261fd856152c629ed
ruzhan <bzruzhan@163.com>
2018-11-10 14:31:53 +0800

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get('/greeting', (req, res) => {
     res.send({hi: 'welcome'});
 });
 
-app.listen(5000, () => {
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Listen port 5000 successfully');
 });
